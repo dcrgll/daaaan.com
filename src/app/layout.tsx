0285c20a16fa05736553a8c3f0a342793141c177
dan <8881965+dcrgll@users.noreply.github.com>
2023-03-09 00:09:01 +0000

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,3 +17,51 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 		</html>
 	)
 }
+
+export const metadata = {
+	title: 'Dan Cargill Builds Things',
+	keywords: ['Dan Cargill', 'React', 'JavaScript', 'Frontend', 'Developer'],
+	creator: 'Dan Cargill',
+	openGraph: {
+		title: 'Dan Cargill Builds Things',
+		description: "You're into websites, I'm good at websites.",
+		url: 'https://www.cargill.dev',
+		siteName: 'Dan Cargill Builds Things',
+		images: [
+			{
+				url: 'https://www.cargill.dev/daaaan-og.png',
+				width: 1920,
+				height: 1080
+			}
+		]
+	},
+	twitter: {
+		title: 'Dan Cargill Builds Things',
+		description: "You're into websites, I'm good at websites.",
+		creator: '@irnbrute_',
+		creatorId: '85074832',
+		card: 'summary_large_image',
+		siteId: '85074832',
+		images: ['https://www.cargill.dev/daaaan-og.png']
+	},
+	icons: {
+		icon: '/icon.png',
+		shortcut: '/shortcut-icon.png',
+		apple: '/apple-touch-icon.png',
+		other: [
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '32x32',
+				url: '/favicon-32x32.png'
+			},
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '16x16',
+				url: '/favicon-16x16.png'
+			}
+		]
+	},
+	manifest: '/site.webmanifest'
+}
