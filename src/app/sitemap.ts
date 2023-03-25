c6e2430ce01fe1db2db6ee4f63705007fd4c4df2
Dan Cargill <8881965+dcrgll@users.noreply.github.com>
2023-03-25 18:26:52 +0000

    Add Next13 Sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,9 @@
+export default function sitemap(){
+
+  const routes = ['', '/about', '/projects', '/hire-me'].map((route) => ({
+    url:`https://www.cargill.dev/${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }))
+
+  return [...routes]
+}
