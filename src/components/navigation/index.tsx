d6f2661b6d05cd8e5fd1ec2027be5728440bfe65
dan <dan@cargill.dev>
2023-03-31 00:33:10 +0100

    Fix Lighthouse Errors

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -18,7 +18,8 @@ const navigationLinks = [
 	},
 	{
 		href: '/cv',
-		label: 'CV'
+		label: 'CV',
+		prefetch: false
 	}
 ]
 
@@ -29,9 +30,14 @@ export default function Navigation() {
 		<aside className="md:-mx-4 md:w-[150px] md:flex-shrink-0  md:px-0">
 			<div className="flex items-center justify-between space-y-2 sm:top-40 md:sticky md:flex-col md:items-start md:space-y-8 lg:top-60">
 				<nav className="sticky flex justify-center space-x-2 sm:mt-[6px] md:flex-col md:justify-start md:space-y-4 md:space-x-0">
-					{navigationLinks.map(({ href, label }) => {
+					{navigationLinks.map(({ href, label, prefetch }) => {
 						return (
-							<NavigationLink href={href} active={pathname === href} key={href}>
+							<NavigationLink
+								href={href}
+								active={pathname === href}
+								key={href}
+								prefetch={prefetch}
+							>
 								{label}
 							</NavigationLink>
 						)
