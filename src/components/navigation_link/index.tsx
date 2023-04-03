6dea4dabcde64b853f944de4c874a2ef0ded4b95
Dan Cargill <dan@cargill.dev>
2023-04-04 00:14:40 +0100

    Add subdomain link
    Remove prefetch
    
    
    Add navigation change

--- src/components/navigation_link/index.tsx
+++ src/components/navigation_link/index.tsx
@@ -5,15 +5,28 @@ export default function NavigationLink({
 	children,
 	href,
 	active,
-	prefetch = true
+	external = false
 }: {
 	children: React.ReactNode
 	href: string
 	active?: boolean
-	prefetch?: boolean
+	external?: boolean
 }) {
+	if (external) {
+		return (
+			<a href={href}>
+				<span
+					className={`px-[10px] py-[5px] ${
+						active ? 'bg-gray-100 text-gray-800 ' : 'dark:text-gray-50'
+					} rounded-md transition duration-300 hover:bg-gray-100  dark:hover:text-gray-800`}
+				>
+					{children}
+				</span>
+			</a>
+		)
+	}
 	return (
-		<Link href={href} prefetch={prefetch}>
+		<Link href={href}>
 			<span
 				className={`px-[10px] py-[5px] ${
 					active ? 'bg-gray-100 text-gray-800 ' : 'dark:text-gray-50'
